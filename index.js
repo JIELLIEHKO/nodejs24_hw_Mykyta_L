819fb50d61aab9d04e0cdc5160eec0c89050b5ed
Mykyta Leshchenko <cs.zevd1999@gmail.com>
2024-09-25 19:19:18 +0300

    hw 1

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 var str = "Node.js course";
 console.log(str.length);
+function multiplyByTwoBasedOnLength(num) {
+    var numStr = num.toString(); // Преобразуем число в строку
+    var length = numStr.length; // Получаем длину строки
+    var result = num; // Инициализируем результат исходным числом
+    // Умножаем число на 2 столько раз, сколько символов в нем
+    for (var i = 0; i < length; i++) {
+        result *= 2;
+    }
+    return result; // Возвращаем результат
+}
+// Пример использования
+var number = 333;
+var result = multiplyByTwoBasedOnLength(number);
+console.log(result); // Вывод: 132
