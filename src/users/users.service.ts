8320ae7ea8847b54afc2b2e62fa13e6099625848
Mykyta Leshchenko <cs.zevd1999@gmail.com>
2024-10-06 17:33:15 +0300

    hw-3 start server

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,31 +5,36 @@ import { IUser } from './interfaces/users.interface';
 
 @Injectable()
 export class UsersService {
-  private users: IUser[] = [];
+  private users: IUser[] = []; // Массив для хранения пользователей
 
+  // Метод для создания пользователя
   create(createUserDto: CreateUserDto) {
     const newUser: IUser = { ...createUserDto };
     this.users.push(newUser);
-    return newUser;
+    return newUser; // Возвращаем созданного пользователя
   }
 
+  // Метод для получения всех пользователей
   findAll() {
-    return this.users;
+    return this.users; // Возвращаем массив пользователей
   }
 
+  // Метод для получения пользователя по ID
   findOne(id: number) {
-    return this.users[id];
+    return this.users[id]; // Возвращаем пользователя по индексу
   }
 
+  // Метод для обновления пользователя
   update(id: number, updateUserDto: UpdateUserDto) {
     const user = this.users[id];
-    this.users[id] = { ...user, ...updateUserDto };
-    return this.users[id];
+    this.users[id] = { ...user, ...updateUserDto }; // Обновляем данные пользователя
+    return this.users[id]; // Возвращаем обновленного пользователя
   }
 
+  // Метод для удаления пользователя
   remove(id: number) {
     const user = this.users[id];
-    this.users.splice(id, 1);
-    return user;
+    this.users.splice(id, 1); // Удаляем пользователя из массива
+    return user; // Возвращаем удаленного пользователя
   }
 }
