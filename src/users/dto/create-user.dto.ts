e4e6c0655e3c657a32b6181ac79cd59a6b40591b
Mykyta Leshchenko <cs.zevd1999@gmail.com>
2024-10-06 17:04:37 +0300

    hw-3 start server

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,16 @@
-export class CreateUserDto {}
+import { IsBoolean, IsInt, IsString, Min } from 'class-validator';
+
+export class CreateUserDto {
+    @IsString()
+    firstName!: string;
+
+    @IsString()
+    lastName!: string;
+
+    @IsInt()
+    @Min(0)
+    age!: number;
+
+    @IsBoolean()
+    isStudent!: boolean;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { IUser } from './interfaces/users.interface';
 
 @Injectable()
 export class UsersService {
+  private users: IUser[] = [];
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const newUser: IUser = { ...createUserDto };
+    this.users.push(newUser);
+    return newUser;
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.users;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.users[id];
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    const user = this.users[id];
+    this.users[id] = { ...user, ...updateUserDto };
+    return this.users[id];
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    const user = this.users[id];
+    this.users.splice(id, 1);
+    return user;
   }
 }
