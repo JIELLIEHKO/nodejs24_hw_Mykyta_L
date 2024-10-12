c7df3272bc95d9704d4a8a4ae7d77d119dc52769
PashchenkoHeorhii <pashchenkoh@mydigicode.com>
2024-10-12 23:54:00 +0300

    remove useless app module + added configuration module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { LogIpMiddleware } from './middlewares/LogIpMiddleware';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LogIpMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
