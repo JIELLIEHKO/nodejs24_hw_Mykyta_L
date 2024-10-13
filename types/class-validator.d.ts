7fc5b5d3ef2031d3ed99b16c2263653b170b24d1
Mykyta Leshchenko <cs.zevd1999@gmail.com>
2024-10-13 15:35:57 +0300

    Remove fix

--- types/class-validator.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'class-validator' {
-    export function IsString(): PropertyDecorator;
-    export function IsInt(): PropertyDecorator;
-    export function IsBoolean(): PropertyDecorator;
-    export function Min(min: number): PropertyDecorator;
-    export function Validate(): PropertyDecorator;
-}
