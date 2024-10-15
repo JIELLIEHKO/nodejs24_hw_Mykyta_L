797fe1c43f68dfdb2b17b19c234bb412cc771a72
Mykyta Leshchenko <cs.zevd1999@gmail.com>
2024-10-15 14:30:18 +0300

    hw-6 start

---
+++ src/Database.ts
@@ -0,0 +1,9 @@
+export abstract class Database {
+    abstract connect(): Promise<void>;
+
+    abstract disconnect(): Promise<void>;
+
+    abstract insertOne(table: string, data: any): Promise<void>;
+
+    abstract findOne<T>(table: string, query: any): Promise<T>;
+}
